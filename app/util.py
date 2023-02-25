b75bc821e265db8be79d137b60244d9144d6c86f
mmk <mmkdhrubo@gmail.com>
2023-02-25 17:26:19 +0600

    updated util.py to make things work again

--- app/util.py
+++ app/util.py
@@ -2,8 +2,11 @@ import pymongo
 import os
 
 
-atlas_password = os.getenv("_ATLAS_PASSWORD")
-atlas_username = os.getenv("_ATLAS_USERNAME")
+# atlas_password = os.getenv("_ATLAS_PASSWORD")
+# atlas_username = os.getenv("_ATLAS_USERNAME")
+
+atlas_password = "1234"
+atlas_username = "user"
 
 client = pymongo.MongoClient(
     f"mongodb+srv://{atlas_username}:{atlas_password}@cluster0.ds5ggbq.mongodb.net/?retryWrites=true&w=majority"
